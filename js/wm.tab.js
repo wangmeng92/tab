dae5da129dd657d0176d0920770fef948621a998
WM <23213876+wangmeng92@users.noreply.github.com>
2018-05-24 14:14:55 +0800

    Add files via upload

--- js/wm.tab.js
+++ js/wm.tab.js
@@ -6,10 +6,10 @@
         var itemWidth = $('.' + mainClass + ' .wm-tab-box').width();
         var index = 0;
 
-        $('.' + mainClass + ' .wm-tab-nav-item:nth-child(1)').addClass('wm-nav-active');
+        $('.' + mainClass + ' .wm-tab-nav-item:eq(0)').addClass('wm-nav-active');
 
-        for (var i = 1; i <= itemNum; i++){
-            $( '.' + mainClass + ' .wm-tab-nav-item:nth-child(' + i + ')' ).attr('data', i);
+        for (var i = 0; i < itemNum; i++){
+            $( '.' + mainClass + ' .wm-tab-nav-item:eq(' + i + ')' ).attr('data', i);
         }
 
         $('.' + mainClass + ' .wm-tab-view').css("width", itemWidth * itemNum + "px");
@@ -17,7 +17,7 @@
 
         $('.' + mainClass + ' .wm-tab-nav-item').click(function(){
             $(this).addClass('wm-nav-active').siblings('.wm-tab-nav-item').removeClass('wm-nav-active');
-            index = $(this).attr('data') - 1;
+            index = $(this).attr('data');
             $('.' + mainClass + ' .wm-tab-view').css('margin-left', -(index * itemWidth));
         });
     }
